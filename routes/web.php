3ddd5841135b3807aa8312e64c4f4db18de03c67
airlanggaadie <airlangga_adie@yahoo.com>
2020-02-12 11:00:07 +0700

    grouping route user

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,11 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/user','UserController@index');
-// Route::get('/user/create','UserController@create');
-Route::post('/user/create','UserController@store');
-Route::post('/user/store','UserController@store');
-Route::get('/user/edit/{id}','UserController@edit');
-Route::put('/user/edit/{id}','UserController@update');
-Route::delete('/user/delete/{id}','UserController@destroy');
+Route::prefix('user')->group(function(){
+    Route::get('/','UserController@index');
+    Route::post('/create','UserController@store');
+    Route::post('/store','UserController@store');
+    Route::get('/edit/{id}','UserController@edit');
+    Route::put('/edit/{id}','UserController@update');
+    Route::delete('/delete/{id}','UserController@destroy');
+});
