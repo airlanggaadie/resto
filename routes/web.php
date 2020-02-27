1026ac0ce12a9715a484b34bfb0986dd83e301ce
airlanggaadie <airlangga_adie@yahoo.com>
2020-02-28 04:43:26 +0700

    maintenance route

--- routes/web.php
+++ routes/web.php
@@ -12,50 +12,44 @@
 */
 
 Route::get('/', function () {
-    return view('pages.resto.bantuan.hubungi');
+    return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::prefix('user')->namespace('Admin')->group(function(){
-    Route::get('/','UserController@index')->middleware(['role:administrator']);
-    Route::get('create','UserController@create');
-    Route::post('store','UserController@store');
-    Route::get('edit/{id}','UserController@edit');
-    Route::put('edit/{id}','UserController@update');
-    Route::delete('delete/{id}','UserController@destroy');
-});
-
-Route::post('tables/create','TablesController@store');
-Route::get('tables/show/{id}','TablesController@show');
-Route::post('tables/update/{id}','TablesController@update');
-Route::delete('tables/delete/{id}','TablesController@destroy');
-
-Route::post('booking/create','BookingController@store');
-Route::get('booking/show/{id}','BookingController@show');
-Route::delete('booking/delete/{id}','BookingController@destroy');
-
-Route::post('resto/create','RestaurantController@store');
-Route::get('resto/show/{id}','RestaurantController@show');
-Route::post('resto/update/{id}','RestaurantController@update');
-Route::delete('resto/delete/{id}','RestaurantController@destroy');
-
-Route::get('/menu', 'MenuController@index');
-
-
-Route::prefix('backend')->namespace('Admin')->group(function(){
-    Route::get('/','DashboardController@index')->name('dashboard');
-// Route::get('/menu', 'MenuController@index')->name('menu');
-});
-
 Route::prefix('admin')->namespace('Admin')->group(function(){
-    Route::get('/menu','MenuController@index')->name('menu');
+    Route::get('/','DashboardController@index')->name('admin-dashboard');
 
+    Route::prefix('user')->group(function(){
+        Route::get('/','UserController@index');
+        Route::get('create','UserController@create');
+        Route::post('store','UserController@store');
+        Route::get('edit/{id}','UserController@edit');
+        Route::put('edit/{id}','UserController@update');
+        Route::delete('delete/{id}','UserController@destroy');
+    });
+
+    Route::post('tables/create','TablesController@store');
+    Route::get('tables/show/{id}','TablesController@show');
+    Route::post('tables/update/{id}','TablesController@update');
+    Route::delete('tables/delete/{id}','TablesController@destroy');
+    
+    Route::post('booking/create','BookingController@store');
+    Route::get('booking/show/{id}','BookingController@show');
+    Route::delete('booking/delete/{id}','BookingController@destroy');
+    
+    Route::post('resto/create','RestaurantController@store');
+    Route::get('resto/show/{id}','RestaurantController@show');
+    Route::post('resto/update/{id}','RestaurantController@update');
+    Route::delete('resto/delete/{id}','RestaurantController@destroy');
+    
+    Route::prefix('menu')->group(function(){
+        Route::get('/','MenuController@index')->name('menu');
+    
+    });
 });
-
-
 
 Route::prefix('resto')->namespace('Resto')->group(function(){
     Route::get('/','HomeController@index')->name('home');
@@ -76,6 +70,3 @@ Route::prefix('resto')->namespace('Resto')->group(function(){
         Route::get('/pusat-bantuan','PusatBantuanController@index')->name('pusatbantuan');
     });
 });
-
-
-// Route::get('/dolar','Resto\Home\HomeController@index');
